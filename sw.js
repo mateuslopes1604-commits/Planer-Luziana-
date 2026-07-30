9c6eef7614bbd81750b3b42fab885dded00ea67c
mateuslopes1604-commits <mateuslopes1604@gmail.com>
2026-07-30 12:07:43 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,57 @@
-const CACHE="planner-luziana-v1";
-const ASSETS=["./","./index.html","./manifest.webmanifest",
-"./assets/cover.png","./assets/segunda.png","./assets/terca.png",
-"./assets/quarta.png","./assets/quinta.png","./assets/sexta.png",
-"./assets/icon-192.png","./assets/icon-512.png"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request))));
+const CACHE_NAME = "planner-luziana-v3";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./assets/capa.png",
+  "./assets/segunda.png",
+  "./assets/terca.png",
+  "./assets/quarta.png",
+  "./assets/quinta.png",
+  "./assets/sexta.png",
+  "./assets/sabado.png",
+  "./assets/domingo.png",
+  "./assets/icon-192.png",
+  "./assets/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES).catch(() => {});
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, copy);
+        });
+
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
